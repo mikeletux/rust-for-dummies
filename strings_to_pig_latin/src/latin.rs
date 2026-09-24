5fd72007b9766764be08122a62142eb2c1727fd5
Miguel Sama <miguelsamamerino@gmail.com>
2026-09-25 00:25:50 +0200

    Refactor functions

--- strings_to_pig_latin/src/latin.rs
+++ strings_to_pig_latin/src/latin.rs
@@ -3,54 +3,34 @@
     // and ay is added, so first becomes irst-fay. Words that start with a vowel have hay added to the 
     // end instead (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
 
-enum Kind {
-    Consonant,
-    Vowel
-}
-
 pub fn string_to_pig_latin(input: &str) -> String {
-    let mut output = String::new();
+    let mut words = Vec::new();
 
-    for word in input.split(' ') {
-        match get_kind_word(word) {
-            Kind::Consonant => {
-                output.push_str(get_consonant_pig_latin_word(word).as_str());
-            },
-            Kind::Vowel => {
-                output.push_str(format!("{}-hay", word).as_str());
-            },
-        }
-        // Add space in between words
-        output.push(' ');
+    for word in input.split_whitespace() {
+        words.push(word_to_pig_latin(word));
     }
-    // Remove last space
-    let trimmed_len = output.trim_end().len();
-    output.truncate(trimmed_len);
 
-    output
+    words.join(" ")
 }
 
-fn get_kind_word(word: &str) -> Kind {
-    let first_letter = word.chars().next().unwrap(); // This might fail lol
+fn word_to_pig_latin(word: &str) -> String {
+    let mut chars = word.chars(); // Iterator
 
-    if first_letter == 'a' || first_letter == 'e' || first_letter == 'i' || first_letter == 'o' || first_letter == 'u' { 
-        Kind::Vowel 
+    let first_char = match chars.next() {
+        Some(c) => c,
+        None => return String::new(),
+    };
+
+    let rest_of_word = chars.as_str();
+
+    if is_vowel(first_char) {
+        format!("{}-hay", word)
     } else {
-        Kind::Consonant
-    } 
+        format!("{}-{}ay", rest_of_word, first_char)
+    }
 }
 
-fn get_consonant_pig_latin_word(word: &str) -> String {
-    let mut consonant_pig_latin_word = String::new();
-    let mut chars = word.chars();
-    let first_char = chars.next().unwrap(); // We assume there are always words.
-
-    for c in chars {
-        // Middle characters go here
-        consonant_pig_latin_word.push(c);
-    }
-
-    consonant_pig_latin_word.push_str(format!("-{}ay", first_char).as_str());
-
-    consonant_pig_latin_word
+fn is_vowel(c: char) -> bool {
+    let vowels = "aeiouáéíóúAEIOUÁÉÍÓÚ";
+    vowels.contains(c)
 }
